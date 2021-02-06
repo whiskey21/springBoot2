23497db6cb0324d966ab569945f9bf07f013930a
lezend22 <lezend22@gmail.com>
2021-02-06 20:08:15 +0900

    add rateDiscountService

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount=1000;
+
+    @Override
+    public int discount(Member member, int price) { //member자체가아닌 Grade만 넘겨도 됨 상황에따라 바꿔써
+        if (member.getGrade() == Grade.VIP) {
+            return discountFixAmount;
+        } else {
+            return 0;
+        }
+    }
+}
