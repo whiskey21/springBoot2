b79792d1a2840170a3d8923a93e1505d8dd70277
lezend22 <lezend22@gmail.com>
2021-02-09 20:16:19 +0900

    add springBeanContainer & fix main

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -3,24 +3,30 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class MemberApp {
+public class OrderApp {
     public static void main(String[] args) {
+
+        //MemberService memberService;
+        //OrderService orderService;
         //AppConfig appConfig = new AppConfig();
-        //MemberService memberService = appConfig.memberService();
+        //memberService = appConfig.memberService();
+        //orderService = appConfig.orderService();
 
         ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
         MemberService memberService = ac.getBean("memberService", MemberService.class);
+        OrderService orderService = ac.getBean("orderService", OrderService.class);
 
-        Member member = new Member(1L, "memberA", Grade.VIP);
+        Member member = new Member(1L, "spring100", Grade.VIP);
         memberService.join(member);
 
-        Member findMember = memberService.findMember(1L);
-        System.out.println("new member = " + member.getName());
-        System.out.println("find member = " + findMember.getName());
+        Order order = orderService.createOrder(1L, "itemSpring", 20000);
+
+        System.out.println("order = " + order);
 
 
     }
